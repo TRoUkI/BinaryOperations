19ddb62acf4e6c322dd70a8f0149fd964686a3e6
TRoUkI <romanjdemchuk@gmail.com>
2023-03-27 22:41:10 +0300

    Little changes on Multiplication

--- src/com/java/binaryMultiplication/BinaryMultiplication.java
+++ src/com/java/binaryMultiplication/BinaryMultiplication.java
@@ -1,10 +1,4 @@
-package com.java.binaryMultiplication;// Write a Java program to multiply two binary numbers.
-//Input Data:
-//Input the first binary number: 10
-//Input the second binary number: 11
-//Expected Output
-//
-//Product of two binary numbers: 110
+package com.java.binaryMultiplication;
 
 public class BinaryMultiplication {
     public static void main(String args[]){
